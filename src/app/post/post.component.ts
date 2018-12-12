00fc2f6f57c3c8fa0397af5b32b78e114581326d
Umut <nuriumut.cakar@hotmail.com>
2018-12-12 21:25:00 +0300

    post ve user için local servis tanımlandı.

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -4,21 +4,25 @@ import { User } from './user';
 import { HttpClient } from '@angular/common/http';
 import { ActivatedRoute } from '@angular/router';
 import { AlertifyService } from '../services/alertify.service'
+import { PostService } from './services/post.service';
+import { UserService } from './services/user.service';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
-  styleUrls: ['./post.component.scss']
+  styleUrls: ['./post.component.scss'],
+  providers: [PostService]
 })
 export class PostComponent implements OnInit {
 
   constructor(
     private http:HttpClient,
     private activatedRoute:ActivatedRoute,
-    private alertifyService:AlertifyService
+    private alertifyService:AlertifyService,
+    private postService:PostService,
+    private userService:UserService
     ) { }
 
-  path:string = "https://jsonplaceholder.typicode.com/"
   posts:Post[]
   users:User[]
   
@@ -31,20 +35,14 @@ export class PostComponent implements OnInit {
   }
 
   getPosts(userId){
-    if(userId){
-      this.http.get<Post[]>(this.path + "posts?userId=" + userId).subscribe(response => {
-        this.posts = response;
-      });
-    } else {
-      this.http.get<Post[]>(this.path + "posts").subscribe(response => {
-        this.posts = response;
-      });
-    }
+    this.postService.getPosts(userId).subscribe(data => {
+      this.posts = data;
+    })
   }
 
   getUsers(){
-    this.http.get<User[]>(this.path + "users").subscribe(response => {
-      this.users = response;
+    this.userService.getUsers().subscribe(data => {
+      this.users = data;
     })
   }
 
