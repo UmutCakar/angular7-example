5087f56e21c900a701078ae7232d0035f9518b1d
Umut <nuriumut.cakar@hotmail.com>
2018-12-18 22:47:48 +0300

    custom pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,6 +9,7 @@ import { CustomerComponent } from './customer/customer.component';
 import { NavComponent } from './nav/nav.component';
 import { PostComponent } from './post/post.component';
 import { AlertifyService } from './services/alertify.service';
+import { PostFilterPipe } from './post/pipes/post-filter.pipe';
 
 @NgModule({
   declarations: [
@@ -15,11 +17,13 @@ import { AlertifyService } from './services/alertify.service';
     CustomerComponent,
     NavComponent,
     PostComponent,
+    PostFilterPipe,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [AlertifyService],
   bootstrap: [AppComponent]
